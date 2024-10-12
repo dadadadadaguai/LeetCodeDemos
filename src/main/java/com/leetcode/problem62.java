b512c278507482bd1036faf099b05f861dd9ffb7
zhouqingkai <1208678644@qq.com>
2024-10-12 10:08:49 +0800

    add:leetCode62题:不同路径

---
+++ src/main/java/com/leetcode/problem62.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+/**
+ * 62:不同路径
+ *
+ * @author dadaguai
+ */
+public class problem62 {
+  public int uniquePaths(int m, int n) {
+      int[][] dp= new int[m][n];
+      dp[0][0]=1;
+      for(int i=0;i<m;i++){
+          for(int j=0;j<n;j++){
+             //考虑边界
+              if(i==0 ||j==0){
+                  dp[i][j]=1;
+              }else {
+                  dp[i][j]=dp[i][j-1]+dp[i-1][j];
+              }
+          }
+      }
+      return dp[m-1][n-1];
+  }
+  public  void dataTest(){
+      int result = uniquePaths(3, 2);
+      System.out.println(result);
+  }
+    public static void main(String[] args){
+        new problem62().dataTest();
+    }
+}
+
