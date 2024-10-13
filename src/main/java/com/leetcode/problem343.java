8c631a33e3caadbba88123f59eaec5d27ff3a947
zhouqingkai <1208678644@qq.com>
2024-10-13 10:40:49 +0800

    新增求解整数拆分问题的动态规划方法
    在项目中添加了新的Java类和方法来解决整数拆分问题，使用动态规划算法找到给定正整数n下的最大乘积。通过迭代的方式，该算法考虑了所有可能的拆分情况，并在每个拆分点上比较了直接乘积和各自的最大乘积，最终返回n的最大乘积。

---
+++ src/main/java/com/leetcode/problem343.java
@@ -0,0 +1,30 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * problem343:整数拆分
+ * @author 12086
+ */
+public class problem343 {
+    public static int integerBreak(int n) {
+        int[] dp= new int[n+1];
+        dp[2]=1;
+        for(int i=3;i<=n;i++){
+            for (int j=1;j<=i/2;j++){
+                dp[i]=Math.max(dp[i],Math.max((i-j)*j,dp[i-j]*j));
+//        System.out.println(dp[i]);
+            }
+        }
+//    System.out.println(Arrays.toString(dp));
+        return dp[n];
+    }
+    public static void dataTest(){
+        int n=2;
+        int i = integerBreak(n);
+    System.out.println(i);
+    }
+    public static void main(String[] args){
+        dataTest();
+    }
+}
