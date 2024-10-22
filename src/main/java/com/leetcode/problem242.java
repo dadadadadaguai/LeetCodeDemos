2bda4a9a758bb1e6f200054c5a2598248c09ef71
zhouqingkai <1208678644@qq.com>
2024-10-22 16:15:06 +0800

    feat: 添加两个新的 LeetCode问题的解决方案
    - 添加 problem242.java，实现有效的字母异位词检测
    - 添加 problem349.java，实现两个数组的交集计算

---
+++ src/main/java/com/leetcode/problem242.java
@@ -0,0 +1,23 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class problem242 {
+  public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+      return false;
+    }
+    char[] charArrays = s.toCharArray();
+    char[] charArrayt = t.toCharArray();
+    Arrays.sort(charArrays);
+    Arrays.sort(charArrayt);
+    return Arrays.equals(charArrays, charArrayt);
+  }
+
+  public static void main(String[] args) {
+    problem242 p = new problem242();
+    System.out.println(p.isAnagram("rat", "car"));
+  }
+}
---
+++ src/main/java/com/leetcode/problem349.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+import java.util.HashSet;
+
+public class problem349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for (int i : nums1) {
+            set1.add(i);
+        }
+        for (int i : nums2) {
+                set2.add(i);
+            }
+        set2.retainAll(set1);
+        return set2.stream().mapToInt(Integer::intValue).toArray();
+        }
+    }
